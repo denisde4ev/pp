df75b4e42811a026ea3d472f3fcc7f9a2807fa7a
Denis Dechev <denisde4ev@vivaldi.net>
2022-08-17 18:33:42 +0300

    a bit refactor of install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@ USR_BIN='/usr/local/bin'
 USR_MAN='/usr/local/share/man/man1'
 
 echo 'Installation requires pandoc and will remove any files at the following locations:'
-printf '\033[31m * %s\033[m\n' "$USR_BIN/pp" "$USR_MAN/pp.1"
+printf '\033[31m * %s\033[m\n' \
+	"$USR_BIN/pp" \
+	"$USR_MAN/pp.1" \
+;
 echo 'and replace them with ones in this repository'
 echo 'Press Enter to continue... (CTRL-C to abort)'
 read -r _
 
-set +x
+set -xeu
 rm -f "$USR_BIN/pp"
-cp ./pp "$USR_BIN/pp"
-
 rm -f "$USR_MAN/pp.1"
+
+cp ./pp "$USR_BIN/pp"
 pandoc --standalone ./pp.1.md --to man > "$USR_MAN/pp.1"
