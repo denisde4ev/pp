7005c8f376df75421a88ea342bf80fb2700d7c53
Denis Dechev (p57) <denisde4ev@vivaldi.net>
2022-10-21 15:20:43 +0300

    on fail to build file then rm it; add chmod +x

--- build.sh
+++ build.sh
@@ -2,16 +2,18 @@
 case $1 in -x) set -x; shift; esac
 cd "${0%%/*}" || exit
 set -eu
-
+x=0
 
 VERSION=$(git describe --long) 2>/dev/null && VERSION=$(printf %s\\n "$VERSION" | sed 's/\([^-]*-\)g/r\1/;s/-/./g') || \
 VERSION=r$(git rev-list --count HEAD).$(git rev-parse --short HEAD) || \
 exit
 
 export VERSION=${VERSION:?}
-./pp.preprocess ./pp.preprocess > ./pp
-
+./pp.preprocess ./pp.preprocess > ./pp || { x=$?; rm -v ./pp; }
+chmod +x ./pp
 
 case ${1-} in --no-doc) ;; *)
-	pandoc --standalone ./pp.1.md --to man  > ./pp.1
+	pandoc --standalone ./pp.1.md --to man  > ./pp.1 || { x=$?; rm -v ./pp.1; }
 esac
+
+exit $x
